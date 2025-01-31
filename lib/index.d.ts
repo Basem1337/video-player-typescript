cd94c1ca3881b63412e487f1a00b86ba5f88d0e9
Basem Mohamed <basem.m1337@gmail.com>
2025-01-31 03:23:21 +0200

    feat: video and basic controls

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+declare var btn: NodeListOf<HTMLButtonElement>;
+declare var inp: NodeListOf<Element>;
+declare var vid: HTMLVideoElement;
+declare var img: HTMLImageElement | null;
+declare var color: Element | null;
+declare var span: HTMLSpanElement | null;
